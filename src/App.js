d5b4dacc6851ecfe5bd0e4e2c1ec6b8628176639
minayar88 <122382410+minayar88@users.noreply.github.com>
2024-03-10 20:39:56 +0000

    Update Href

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className='container'>
         <Weather defaultCity="New York"/>
-         <p>This project was coded by <a href="#" target="_blank" rel="noreferrer">Mina</a> and is <a href="https://github.com/minashahramyar/react-weather-app" target='_blank' rel="noreferrer">open sourced on Github</a></p>
+         <p>This project was coded by <a href="https://github.com/minashahramyar/react-weather-app" target="_blank" rel="noreferrer">Mina</a> and is <a href="https://github.com/minashahramyar/react-weather-app" target='_blank' rel="noreferrer">open sourced on Github</a></p>
       </div>
     </div>
   );
